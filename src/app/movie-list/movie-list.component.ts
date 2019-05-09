fd8ff413120a79d80b279bd3f28424e164b0d242
Carla Agüero <c.agueromiranda@gmail.com>
2019-05-09 12:46:02 +0200

    movie-list read url param

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.scss']
 })
-export class MovieListComponent implements OnInit {
-
-  constructor() { }
+export class MovieListComponent implements OnInit, OnDestroy {
+  type:string;
+  typeSubscription:any;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.typeSubscription = this.route.params.subscribe(params => {
+      this.type = params.type
+    })
+  }
+  ngOnDestroy(){
+    this.typeSubscription.unsubscribe();
   }
 
 }
